0dbf6f562c81f3f82024de79148d5cc4487b2e60
Temi <temi0072@gmail.com>
2024-03-26 11:22:34 +0200

    Added exception handling

--- FileSorter.py
+++ FileSorter.py
@@ -3,14 +3,19 @@ import shutil
 
 def main():
     path = input("Enter a path: ")
-    files = os.listdir(path) #Gets list of all files in a directory
-    for file in files:
-        filename, extension = os.path.splitext(file)
-        extension = extension[1:]
-        if not(os.path.exists(path+"/"+extension)):
-            os.mkdir(path+"/"+extension)
-        shutil.move(path+"/"+ file, path+"/"+extension+"/"+file)
-    print("Done")
+    try:
+        files = os.listdir(path) #Gets list of all files in a directory
+        for file in files:
+            filename, extension = os.path.splitext(file)
+            extension = extension[1:]
+            if not(os.path.exists(path+"/"+extension)):
+                os.mkdir(path+"/"+extension)
+            shutil.move(path+"/"+ file, path+"/"+extension+"/"+file)
+        print("Done")
+    except Exception as e:
+        print("An error occured: ", e)
+
+
 
 if __name__ == "__main__":
     main()
